8119bfda7cbf5a0206322bfa6893bc0063645cd6
s42410617 <s42410617@github.com>
2026-06-16 00:28:53 +0800

    添加PWA支持，创建APP图标和Service Worker

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'stock-report-v1';
+const CACHE_ASSETS = [
+    '/',
+    '/index.html',
+    '/A股行业板块轮动分析报告.html'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(CACHE_ASSETS);
+        }).then(() => {
+            self.skipWaiting();
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        }).then(() => {
+            self.clients.claim();
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request).then((networkResponse) => {
+                if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+                    return networkResponse;
+                }
+                const responseToCache = networkResponse.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseToCache);
+                });
+                return networkResponse;
+            });
+        })
+    );
+});
